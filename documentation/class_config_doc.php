32253bd791e780334c4ab3003f98f9306c35cbb6
michaelhodgejr <mikeh@lightsky.com>
2010-07-25 16:51:58 -0500

    Added Config Class Documenation

---
+++ documentation/class_config_doc.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *STAT Configuration Class Documenation
+ *This is the first class loaded, and parses YAML files for config/internationalization
+ *as well as setting various php variables needed  
+ */
+ 
+config {
+
+/* Properties */
+
+public $db      /* The main DB Object for STAT */
+public $config  /* The config variable housing the parsed YAML config file */
+public $lang    /* The internationalization file */
+
+
+/* Methods */
+
+public __construct(void) /*Initiates the class */
+private initiate_db(void)  /*This intiates the $db varible using params parsed from the config file */
+private parse_yaml_files(void) /* This parses the config YAML and Internationalization YAML files. Sets the $config, $lang properties*/
+private set_php_vars(void)  /* This sets any required PHP Variables for the STAT System*/
+
+
+} 
+?>

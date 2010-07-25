94b86938c55eb3a9df3b1e97b50e9c5c851acdb8
michaelhodgejr <mikeh@lightsky.com>
2010-07-25 17:15:05 -0500

    Added Main Class documentation

---
+++ documentation/class_main_doc.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *STAT Main Class Documenation
+ *This is the Main Stat class loaded, and extends the config class. All other classes extend this one
+ *Contains core funcationality for the program 
+ */
+ 
+main extends config {
+
+/* Properties */
+
+
+
+/* Methods */
+
+public __construct(void) /*Initiates the class */
+string public clean_input($string) /* This string preforms a real-escape-string and htmlentities and returns string*/
+
+} 
+?>

d67d2e3ec954872f1fa5098efad9e01353972015
michaelhodgejr <mikeh@lightsky.com>
2010-07-27 17:38:29 -0700

    Reverted File

--- documentation/class_config_doc.php
+++ documentation/class_config_doc.php
@@ -1,36 +1,27 @@
 <?php
 
 /**
- *STAT Note Class Documenation
- *This class is the main note taking class for STAT. It extends main 
+ *STAT Configuration Class Documenation
+ *This is the first class loaded, and parses YAML files for config/internationalization
+ *as well as setting various php variables needed and substantiating db object 
  */
  
-
-note extends main{
+config {
 
 /* Properties */
 
-int public $id 			 /* The auto inc database id */
-int public $cid 		 /* CID of the student */
-string public $fname 		 /* The first name of the student */
-string public $lname 		 /* The last name of the student */
-timestamp public $date		 /* unix timestamp of the date */
-int public $category_id  	 /* category id */
-string public $summary  	 /* summary of note */
-string public $user    		 /* person submitting the note */
-int public $duration   		 /* duration of training session (in seconds) */
-string public $training_items 	 /* Items covered on the training notes */
-array public $note_error	 /* Errors created for note class */
+public $db      /* The main DB Object for STAT */
+public $config  /* The config variable housing the parsed YAML config file */
+public $lang    /* The internationalization file */
+
 
 /* Methods */
 
-void public __construct(int $id) /* This constructs the function and sets note properties if id is provided */
-void public retrieve_note(void)  /* This function actually does the pull from the database for the note */
-void public save_note(void)	 /* This function saves the note to the database */
-void public update_note(void)    /* This function updates the note in the database */
-void public delete_note(void)    /* This function "deletes" a note */
+public __construct(void) /*Initiates the class */
+private initiate_db(void)  /*This intiates the $db varible using params parsed from the config file */
+private parse_yaml_files(void) /* This parses the config YAML and Internationalization YAML files. Sets the $config, $lang properties*/
+private set_php_vars(void)  /* This sets any required PHP Variables for the STAT System*/
 
-}
 
 } 
 ?>

314de64593704d8b85c7719660d24d621a6b48b1
michaelhodgejr <mikeh@lightsky.com>
2010-08-02 19:24:32 -0500

    Began work on configuration class, autoloader function, and base.php file

---
+++ includes/base.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Base File 
+ * @package stat
+ * @version 1.0
+ * @desc This file initates auto loading function for the classes and nessicary includes
+ */
+
+/**
+ * @todo Set .htaccess file to auto_prepend base.php
+ */
+
+/**
+ * Check Version of PHP. Minimum Version Required is 5.0
+ */
+
+if(phpversion() < "5.0"){
+  die("Your version of PHP is " . phpversion() . ". STAT requires a minimum PHP version of 5.0");
+}
+
+/**
+ * @param string $class Class name
+ */
+function __autoload($class){
+	require_once 'frameworks/sfyaml.php';
+	require_once  'classes/' . $class;
+}
+?>
---
+++ includes/classes/configuration.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *Configuration Class
+ *@package stat
+ *@version 1.0
+ *@desc This file contains loads the configuration parameters and internationalization files
+ */
+class configuration {
+
+ /*Properties*/
+
+ /**
+  * @global lang
+  * @desc Contains language array
+  */
+ public $lang;
+
+ /**
+  * @global db
+  * @desc Contains the database objects
+  */
+
+ public $db;
+
+ /**
+  * @global config
+  * @desc Contains the configuration parameters
+  */
+
+ public $config;
+
+
+ /**
+   * @method configuration::__construct()
+   * @desc Initalizes the configuration class
+   */
+  public function __construct(){
+    $this->parse_yaml(); 
+    $this->init_db(); 
+    $this->set_php_vars();
+  }
+
+  /**
+   * @method configuration::init_db
+   * @desc This function initates the database variables
+   * @returns Array of database objects
+   */
+  private function init_db(){
+     /**
+      * @todo Initalize database informations based upon presence of DBA
+      */
+  }
+
+  /**
+   * @method configuration::parse_yaml
+   * @desc Parses the various YAML files required for use
+   * @return array Returns associative array of parsed config and internationalization file
+   */
+  private function parse_yaml(){
+    $yml = new sfYamlParser();
+
+    try {
+      $this->config = $yml->parse(file_get_contents('../config/config.yaml'));
+    }
+
+    catch (InvalidArgumentException $e){
+      die("Unable to parse the STAT Configuration File: ".$e->getMessage());
+    }
+
+    try{
+      $this->lang = $yml->parse(file_get_contents('../l18n/' . $config['LANG'] . '.yml'));
+    }
+
+    catch (InvalidArgumentException $e){
+      die("Unable to parse the STAT Language file: ".$e->getMessage());
+    }
+  }
+
+  /**
+   * @method configuration::parse_yaml
+   * @desc This method sets misc PHP variables
+   * @return true
+   */
+  private set_php_vars(){
+    if(phpversion() < 5.0){
+      die("Your version of PHP is " . phpversion() . ". STAT requires a minimum PHP version of 5.0");
+    }
+  }
+}
+?>
